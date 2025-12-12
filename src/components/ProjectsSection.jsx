021d615e779723b52e8e5e12432330edd1058044
elmehdi <elmehdi.sadki@outlook.fr>
2025-12-12 21:57:57 +0100

    t

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,8 +1,8 @@
 import { ArrowRight, ExternalLink, Github } from "lucide-react";
-import Project1 from "@/assets/projects/Project1.png";
-import Project2 from "@/assets/projects/Project2.png";
-import Project3 from "@/assets/projects/Project3.png";
-import Project4 from "@/assets/projects/Project4.png";
+import Project1 from "@/assets/projects/project1.png";
+import Project2 from "@/assets/projects/project2.png";
+import Project3 from "@/assets/projects/project3.png";
+import Project4 from "@/assets/projects/project4.png";
 const projects = [
   {
   id: 1,
